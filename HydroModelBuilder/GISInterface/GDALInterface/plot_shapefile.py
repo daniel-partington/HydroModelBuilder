e223d6afed349550605a8e4358aadfac4bbe2543
daniel-partington <dpartington1982@gmail.com>
2016-08-18 15:58:09 +0930

    extra GIS utilities

---
+++ HydroModelBuilder/GISInterface/GDALInterface/plot_shapefile.py
@@ -0,0 +1,93 @@
+# Taken from http://stackoverflow.com/questions/15968762/shapefile-and-matplotlib-plot-polygon-collection-of-shapefile-coordinates
+
+from osgeo import ogr
+import numpy as np
+import matplotlib.path as mpath
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+
+def plotShpfile(shpfile=None, shptype=None, facecolor='gray', edgecolor='black'):
+    
+    if type(shpfile) == str:
+        shpfile = [shpfile]
+        shptype = [shptype]
+    
+    for index, shp in enumerate(shpfile):
+        # Extract first layer of features from shapefile using OGR
+        ds = None        
+        
+        ds = ogr.Open(shpfile[index])
+        nlay = ds.GetLayerCount()
+        lyr = ds.GetLayer(0)
+        
+        if index == 0:
+            # Get extent and calculate buffer size
+            ext = lyr.GetExtent()
+            xoff = (ext[1]-ext[0])/50
+            yoff = (ext[3]-ext[2])/50
+            
+            # Prepare figure
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.set_xlim(ext[0]-xoff,ext[1]+xoff)
+            ax.set_ylim(ext[2]-yoff,ext[3]+yoff)
+
+        shptype2 = shptype[index]        
+        if shptype2 == 'points':
+            for feat in lyr:
+                geomRef = feat.GetGeometryRef()
+                x = [geomRef.GetX(i) for i in range(geomRef.GetPointCount())] #although points has only one object..
+                y = [geomRef.GetY(i) for i in range(geomRef.GetPointCount())] #although points has only one object..
+                i1, = ax.plot(x,y, 'ro', alpha=0.5)
+            #ax.set_aspect(1.0)
+            #plt.show()
+            
+        if shptype2 == 'polyline':
+            for feat in lyr:
+                geomRef = feat.GetGeometryRef()
+                x = [geomRef.GetX(i) for i in range(geomRef.GetPointCount())]
+                y = [geomRef.GetY(i) for i in range(geomRef.GetPointCount())]
+                i2, = ax.plot(x,y, color='blue')       
+            #ax.set_aspect(1.0)
+            #plt.show()
+           
+        if shptype2 == 'polygon':
+            paths = []
+            lyr.ResetReading()
+            
+            # Read all features in layer and store as paths
+            for feat in lyr:
+                geom = feat.geometry()
+                codes = []
+                all_x = []
+                all_y = []
+                for i in range(geom.GetGeometryCount()):
+                    # Read ring geometry and create path
+                    r = geom.GetGeometryRef(i)
+                    x = [r.GetX(j) for j in range(r.GetPointCount())]
+                    y = [r.GetY(j) for j in range(r.GetPointCount())]
+                    # skip boundary between individual rings
+                    codes += [mpath.Path.MOVETO] + \
+                                 (len(x)-1)*[mpath.Path.LINETO]
+                    all_x += x
+                    all_y += y
+                path = mpath.Path(np.column_stack((all_x,all_y)), codes)
+                paths.append(path)
+            
+            # Add paths as patches to axes
+            for path in paths:
+                patch = mpatches.PathPatch(path, \
+                        facecolor=facecolor, edgecolor=edgecolor, alpha=0.5)
+                ax.add_patch(patch)
+        
+    ax.set_aspect(1.0)
+    plt.show()
+    
+if __name__ == "__main__":
+    shpfile0 = r"C:\Workspace\part0075\MDB modelling\testbox\01_steady_state\GW_model_area_model.shp"
+    #plotShpfile(shpfile=shpfile, shptype='polygon')
+    shpfile1 = r"C:\Workspace\part0075\MDB modelling\testbox\01_steady_state\Campaspe_Riv_model.shp"
+    #plotShpfile(shpfile=shpfile, shptype='polyline')
+    shpfile2 = r"C:\Workspace\part0075\MDB modelling\testbox\01_steady_state\pumping wells_reproj.shp"
+    
+    plotShpfile(shpfile=[shpfile0, shpfile1, shpfile2], shptype=['polygon', 'polyline', 'points'])    
