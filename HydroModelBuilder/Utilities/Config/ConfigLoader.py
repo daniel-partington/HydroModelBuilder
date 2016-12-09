28a3f3024c232adbfad18b0c49955e2b6805efe8
Takuya Iwanaga <iwanaga.takuya@anu.edu.au>
2016-12-09 12:46:56 +1100

    Updated ConfigLoader to handle multiple models and workspace settings
    Falls back to original behaviour if no config file is specified.

--- HydroModelBuilder/Utilities/Config/ConfigLoader.py
+++ HydroModelBuilder/Utilities/Config/ConfigLoader.py
@@ -1,29 +1,50 @@
 """
 Configuration Loader.
-
-Adapted from Integrated Framework - Core Package
 """
 
 import os
 import sys
+from jsmin import jsmin
 import json
+import subprocess
 
-
-class ConfigLoader():
+class ConfigLoader(object):
     """
     Configuration Loader.
 
     Loads model configuration setup from json files
     """
 
-    def __init__(self):
+    def __init__(self, config_file=None):
         """Configuration Loader Constructor."""
         self.model_config = {}
         sep = os.path.sep
 
+        if config_file is not None:
+            here = os.path.dirname(config_file)
+            try:
+                with open(config_file) as _invalidJSON:
+                    temp = jsmin(_invalidJSON.read());
+                # End with
+            except Exception:
+                sys.exit("Error reading config file. Could be due to incorrect JSON formatting")
+            # End try
+
+            self.model_config = json.loads(temp)
+            return
+        else:
+            # Maintain previous behaviour
+            # Open config file from wherever current directory is
+            # here = os.path.dirname(__file__)
+            here = "" # os.path.dirname(__file__)
+            config_file = 'model_config.json'
+        # End if
+
         try:
-            self.model_config = json.load(
-                open(os.path.join(os.path.dirname('__file__'), 'model_config.json')))
+            with open(config_file) as _invalidJSON:
+                temp = jsmin(_invalidJSON.read());
+                self.model_config = json.loads(temp)
+            # End with
 
             # Check that working directory exists and ends with slash
             try:
@@ -47,14 +68,17 @@ class ConfigLoader():
                 print("WARNING: Invalid working directory, defaulting to current directory")
 
                 # If any error occurs, default to old behaviour
-                self.model_config["working_dir"] = os.path.dirname('__file__')
+                self.model_config["working_dir"] = here
             # End try
 
         except Exception as e:
             # If any other error occurs, default to old behaviour
-            self.model_config["working_dir"] = os.path.dirname('__file__')
+            self.model_config["working_dir"] = here
 
-            print e
+            # Hide exception msg if this has resorted to default behaviour
+            if config_file == here:
+                print e
+            # End if
 
         # End try
 
@@ -71,6 +95,53 @@ class ConfigLoader():
         # End if
 
     # End __init__()
+
+    def set_environment(self, project_name, user_env_name=None):
+        """
+        Set intended project environment. Name must match one of those defined in the config file.
+
+        In addition to changing configurations, this also changes directories to the project folder
+        as given in the config file.
+
+        :param user_env_name: str, name of the user environment.
+                              If not given (i.e. set to None), uses the logged in username.
+                              Defaults to None.
+        """
+        if user_env_name is None:
+            user_env_name = subprocess.check_output("whoami").replace("\r\n", "")
+
+        self.settings = self.model_config[project_name]["environment"][user_env_name]
+        self.model_config = self.model_config[project_name]["environment"][user_env_name]
+
+        for v, var in self.settings.iteritems():
+            setattr(self, v, var)
+        # End for
+
+        proj_folder = self.settings.get("project_folder", "")
+
+        if len(proj_folder) > 0:
+            os.chdir(proj_folder)
+        # End if
+
+        return self
+    # End set_environment()
+
+    def get_setting(self, params):
+        """
+        Retrieve value from settings.
+
+        :param params: List, list of parameters to drill down
+                       e.g. ["models", "Climate", "path"] will get value(s) for
+                            CONFIG.settings["data"]["Climate"]["path"]
+
+        :returns: object, value for specified parameter
+        """
+        temp = self.settings
+        for p in params:
+            temp = temp.get(p, {})
+        # End for
+        return temp
+    # End get_setting()
 
 # End ConfigLoader()
 
