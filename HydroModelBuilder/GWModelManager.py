a517f59ad0a3da2e4fe5b241b83b7558732429ea
Takuya Iwanaga <takuyai@gmail.com>
2018-01-23 19:24:40 +1100

    Added doc strings and deprecation warnings

--- HydroModelBuilder/GWModelManager.py
+++ HydroModelBuilder/GWModelManager.py
@@ -1,4 +1,5 @@
 import cPickle as pickle
+import warnings
 
 from HydroModelBuilder.GWModelBuilder import GWModelBuilder
 
@@ -58,12 +59,24 @@ class GWModelManager(object):
 
     # Save and load utility using pickle
     def save_obj(self, obj, filename):
-        with open(filename + '.pkl', 'wb') as f:
+        """
+        Save object to pickle file.
+
+        :param obj: object, to save.
+        :param filename: str, filename to save object to
+        """
+        filename = filename + ".pkl" if not filename.endswith(".pkl") else filename
+        with open(filename, 'wb') as f:
             pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
         # End with
     # End save_obj()
 
     def load_obj(self, filename):
+        """
+        Load object from pickle file.
+
+        :param filename: str, name of file to load object from.
+        """
         if filename.endswith('pkl'):
             with open(filename, 'rb') as f:
                 return pickle.load(f)
@@ -71,12 +84,15 @@ class GWModelManager(object):
             raise TypeError('File type not recognised as "pkl"')
         # End if
 
-    def build_model_database(self):
-        pass
-
     def build_GW_model(self, name=None):
+        """
+        Generate a groundwater model using `GWModelBuilder`
+
+        :param name: str, name of model. If `None` given (default) then name shall be 'default' and its number in the
+                     model register.
+        """
         if name is None:
-            name = 'default{}'.format(self.models)
+            name = 'default{}'.format(self.models + 1)
         self.name = name
 
         self.GW_build[name] = GWModelBuilder(name=None, model_type=None,
@@ -86,8 +102,10 @@ class GWModelManager(object):
                                              GISInterface=None,
                                              data_format='binary',
                                              target_attr=self.target_attr)
+    # End build_GW_model()
 
     def emulate_GW_model(self, emulation_method):
+        raise NotImplementedError("Not yet implemented.")
         # Create new GW_model using emulation of existing model
         emulation_methods = ['polynomial chaos expansions', 'PCE', 'multi-fidelity', 'M-F']
         if emulation_method in emulation_methods:
@@ -95,42 +113,61 @@ class GWModelManager(object):
                 pass
             elif emulation_method in ['multi-fidelity', 'M-F']:
                 pass
+            # End if
+        # End if
+    # End if
 
     def updateParameters(self, model_name, parameters):
+        """
+        Update parameters in the groundwater model.
 
+        :param model_name: str, name of model to update.
+        :param parameters: str or dict, parameter(s) to update. If string, assume it is a file to load data from.
+        """
+        warnings.warn("DEPRECATED. Use `update_parameters` instead.", DeprecationWarning)
         params_new = {}
         if type(parameters) == str:
-            """
-            Assume string is a filename to load in the format:
-            Header: PARNAME PARVAL
-            Rows:   p1      val
-            """
+            # Assume string is a filename to load in the format:
+            # Header: PARNAME PARVAL
+            # Rows:   p1      val
             with open(parameters, 'r') as f:
                 text = f.readlines()
                 for line in text:
                     par, val = line.strip('\n').split('\t')
                     params_new[par] = val
-
+                # End for
+            # End with
         elif type(parameters) == dict:
-            """
-            Assume parameter names match those defined in the model
-            e.g. if params are 'p1' and 'p2' expect e.g. {'p1':0.1, 'p2':3.2}
-            """
+            # Assume parameter names match those defined in the model
+            # e.g. if params are 'p1' and 'p2' expect e.g. {'p1':0.1, 'p2':3.2}
             params_new = parameters
+        # End if
 
         # Check parameter passed against existing parameters in the model
         original_param_names = self.GW_build[model_name].parameters.param.keys()
         differences = list(set(original_param_names) - set(params_new.keys()))
         if len(differences) > 0:
-            print 'The following parameters were no matched: ', differences
+            print 'The following parameters were not matched: ', differences
         # End if
-        for key in params_new.keys():
+
+        for key in params_new:
             if key not in original_param_names:
-                print 'Parameter name passed not matched, hence not assigned: ', key
+                print('Parameter name passed not matched, hence not assigned: ', key)
             elif key in original_param_names:
                 self.GW_build[model_name].parameters.param[key] = params_new[key]
             # End if
-        # end for
+        # End for
+    # End updateParameters()
+
+    def update_parameters(self, model_name, parameters):
+        """
+        Update parameters in the groundwater model.
+
+        :param model_name: str, name of model to update.
+        :param parameters: str or dict, parameter(s) to update. If string, assume it is a file to load data from.
+        """
+        self.updateParameters(model_name, parameters)
+    # End update_parameters()
 
     def setupPEST(self, model_name, directory=None, csv_copy=False, excel_copy=False, models_ID=None):
         from HydroModelBuilder.Utilities.PESTInterface.PESTInterface import PESTInterface
@@ -152,21 +189,27 @@ class GWModelManager(object):
     # End models()
 
     def load_GW_model(self, GW_model, out_data_folder=None):
-        """Load groundwater model."""
+        """
+        Load groundwater model from pickle file.
+
+        :param GW_model: str, filename of pickled object to load
+        :param out_data_folder: str, OPTIONAL where outputs are written to.
+        """
         packaged_model = self.load_obj(GW_model)
+        pkg_model_name = packaged_model['name']
 
-        self.GW_build[packaged_model['name']] = GWModelBuilder(name=packaged_model['name'],
-                                                               model_type=packaged_model['model_type'],
-                                                               mesh_type=packaged_model['_mesh_type'],
-                                                               units=packaged_model['_units'],
-                                                               data_folder=packaged_model['data_folder'],
-                                                               out_data_folder=packaged_model['out_data_folder'],
-                                                               model_data_folder=packaged_model['model_data_folder'],
-                                                               GISInterface=None,
-                                                               data_format=packaged_model['data_format'],
-                                                               target_attr=self.target_attr)
+        self.GW_build[pkg_model_name] = GWModelBuilder(name=packaged_model['name'],
+                                                       model_type=packaged_model['model_type'],
+                                                       mesh_type=packaged_model['_mesh_type'],
+                                                       units=packaged_model['_units'],
+                                                       data_folder=packaged_model['data_folder'],
+                                                       out_data_folder=packaged_model['out_data_folder'],
+                                                       model_data_folder=packaged_model['model_data_folder'],
+                                                       GISInterface=None,
+                                                       data_format=packaged_model['data_format'],
+                                                       target_attr=self.target_attr)
 
-        ref_pkg_model = self.GW_build[packaged_model['name']]
+        ref_pkg_model = self.GW_build[pkg_model_name]
         for key in self.target_attr:
             setattr(ref_pkg_model, key, packaged_model[key])
         # End for
@@ -175,11 +218,12 @@ class GWModelManager(object):
     # End load_GW_model()
 
     def load_GW_models(self, GW_models):
+        """
+        Load given list of groundwater model pickle files.
+
+        :param GW_models: list[str], pickle files to load in.
+        """
         for model in GW_models:
-            self.model = self.load_GW_model(model)
-
-    def GW_model_runner(self):
-        pass
-
-    def create_GW_model_copy(self):
-        pass
+            self.load_GW_model(model)
+        # End for
+    # End load_GW_models()
