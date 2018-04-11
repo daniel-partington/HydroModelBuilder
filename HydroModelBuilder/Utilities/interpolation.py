9a20dc1e186c09ff3be275000fb0ebefb82bca1e
daniel-partington <dpartington1982@gmail.com>
2018-04-11 16:40:41 +0930

    Added Gaussian Processes to the interpolation methods

--- HydroModelBuilder/Utilities/interpolation.py
+++ HydroModelBuilder/Utilities/interpolation.py
@@ -6,7 +6,7 @@ Created on Wed May 11 21:21:51 2016
 
 import numpy as np
 from scipy.interpolate import Rbf, griddata
-
+from sklearn.gaussian_process import GaussianProcess
 
 def GridData(points, values, xi, method='nearest'):
     """Options for griddata method are ['nearest', 'linear', 'cubic']
@@ -21,10 +21,10 @@ def GridData(points, values, xi, method='nearest'):
     """
 
     if method in ['linear', 'cubic']:
-        z1 = griddata(points, values, xi, method=method)
-        z0 = griddata(points, values, xi, method='nearest')
-        z2 = z1
-        z2[np.isnan(z1)] = z0[np.isnan(z1)]
+        z2 = griddata(points, values, xi, method=method)
+        mask = np.isnan(z2)
+        z2[mask] = np.interp(np.flatnonzero(mask), np.flatnonzero(~mask), z2[~mask])
+        
     else:
         z2 = griddata(points, values, xi, method=method)
     # End if
@@ -65,8 +65,19 @@ def RadialBasisFunctions3D(points, values, xi, function='multiquadric', epsilon=
     return ValuesI
 # End RadialBasisFunctions3D()
 
-
-def Interpolator(mesh_type, points, values, to_array, method='linear', use='griddata', function='multiquadric', epsilon=2):
+def GaussianProcess_interp(points, values, xi, theta0=0.1, thetaL=0.001, thetaU=1.0, nugget=0.01):
+    x_points = np.array(points)[:, 0]
+    y_points = np.array(points)[:, 1]
+    gp = GaussianProcess(theta0=theta0, thetaL=thetaL, thetaU=thetaU, nugget=nugget)
+    gp.fit(X=np.column_stack([x_points, y_points]), y=np.array(values))
+    xi_as_cols = np.column_stack([xi[0].flatten(), xi[1].flatten()])
+    z = gp.predict(xi_as_cols).reshape(xi[0].shape)
+    return z
+# End GaussianProcess
+    
+def Interpolator(mesh_type, points, values, to_array, method='linear', 
+                 use='griddata', function='multiquadric', epsilon=2, theta0=0.1, 
+                 thetaL=0.001, thetaU=1.0, nugget=0.01):
     """
 
     :param mesh_type:
@@ -83,7 +94,13 @@ def Interpolator(mesh_type, points, values, to_array, method='linear', use='grid
         if use == 'griddata':
             to_values = GridData(points, values, to_array, method=method)
         elif use == 'rbf':
-            to_values = RadialBasisFunctions2D(points, values, to_array, function=function, epsilon=epsilon)
+            to_values = RadialBasisFunctions2D(points, values, to_array, 
+                                               function=function, 
+                                               epsilon=epsilon)
+        elif use == 'gaussian_process':
+            to_values = GaussianProcess_interp(points, values, to_array, 
+                                               theta0=theta0, thetaL=thetaL, 
+                                               thetaU=thetaU, nugget=nugget)
         else:
             print 'Interpolation "use" not recognised'
         # End if
@@ -143,47 +160,56 @@ if __name__ == "__main__":
     grid_z1 = Interpolator('structured', points, values, xi, method='linear')
     grid_z2 = Interpolator('structured', points, values, xi, method='cubic')
 
-    plt.subplot(221)
+    plt.subplot(231)
     plt.imshow(func(grid_x, grid_y).T, extent=(0, 1, 0, 1), origin='lower')
     plt.plot(points[:, 0], points[:, 1], 'k.', ms=1)
     plt.title('Original')
-    plt.subplot(222)
-    plt.imshow(grid_z0.T, extent=(0, 1, 0, 1), origin='lower')
+    plt.subplot(232)
+    plt.imshow(grid_z0.T, extent=(0, 1, 0, 1), origin='lower', interpolation='none')
     plt.title('Nearest')
-    plt.subplot(223)
-    plt.imshow(grid_z1.T, extent=(0, 1, 0, 1), origin='lower')
+    plt.subplot(233)
+    plt.imshow(grid_z1.T, extent=(0, 1, 0, 1), origin='lower', interpolation='none')
     plt.title('Linear')
-    plt.subplot(224)
-    plt.imshow(grid_z2.T, extent=(0, 1, 0, 1), origin='lower')
+    plt.subplot(234)
+    plt.imshow(grid_z2.T, extent=(0, 1, 0, 1), origin='lower', interpolation='none')
     plt.title('Cubic')
     plt.gcf().set_size_inches(6, 6)
-    plt.show()
+    #plt.show()
 
     # Test for radial basis functions
 
     x = np.random.rand(100) * 4.0 - 2.0
     y = np.random.rand(100) * 4.0 - 2.0
-    z = x * np.exp(-x**2 - y**2)
+#    z = x * np.exp(-x**2 - y**2)
 
-    x = np.array(points)[:, 0]
-    y = np.array(points)[:, 1]
+#    x = np.array(points)[:, 0]
+#    y = np.array(points)[:, 1]
 
     # 2-d tests - setup scattered data
-    ti = np.linspace(-2.0, 2.0, 100)
-    XI, YI = np.meshgrid(ti, ti)
-    epsilon = 0.01
+#    ti = np.linspace(-2.0, 2.0, 100)
+#    XI, YI = np.meshgrid(ti, ti)
+    epsilon = 5
 
-    ZI = Interpolator('structured', points, values, xi, use='rbf', function='multiquadric', epsilon=epsilon)
+    methods = ['multiquadric', 'inverse', 'gaussian', 'linear', 'cubic', 'quintic', 'thin_plate']
+    grid_z3 = Interpolator('structured', points, values, xi, use='rbf', function='thin_plate', epsilon=epsilon)
 
-    plt.figure()
-    plt.subplot(1, 1, 1)
+    #plt.figure()
+    plt.subplot(235)
     #plt.pcolor(XI, YI, ZI, cmap=cm.jet)
-    plt.pcolor(grid_x, grid_y, ZI, cmap=cm.jet)
+    
+    plt.imshow(grid_z3.T, extent=(0, 1, 0, 1), origin='lower', interpolation='none')
+    #plt.pcolor(grid_x, grid_y, ZI, cmap=cm.jet)
     #plt.scatter(x, y, 100, z, cmap=cm.jet)
-    plt.scatter(x, y, 40, values, cmap=cm.jet)
+    #plt.scatter(x, y, 20, values, cmap=cm.jet)
     plt.title('RBF interpolation - multiquadrics')
     #plt.xlim(-2, 2)
     #plt.ylim(-2, 2)
     plt.xlim(0, 1)
     plt.ylim(0, 1)
-    plt.colorbar()
+    #plt.colorbar()
+
+    grid_z3 = Interpolator('structured', points, values, xi, use='gaussian_process',  theta0=0.1, thetaL=0.001, thetaU=10.0, nugget=0.01)
+    plt.subplot(236)
+    plt.imshow(grid_z3.T, extent=(0, 1, 0, 1), origin='lower', interpolation='none')
+    plt.title('Gaussian Process')
+    
